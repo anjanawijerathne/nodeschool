a13a4ea7552418ac9766b5cda55089945e77d263
Anjana Wijerathne <anjanawijerathne@gmail.com>
2017-03-06 22:32:19 +0530

    Hex

---
+++ four.js
@@ -0,0 +1,9 @@
+
+var fs = require('fs');
+if (process.argv.length == 3) {    
+    var buffer = fs.readFileSync(process.argv[2]);
+    var stringBuf = buffer.toString();
+    console.log (stringBuf.split('\n').length - 1);
+} else {
+    console.log ('invalid arguments');
+}
